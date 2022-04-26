9c05f7183e4bdfa49da49c550decd707c4e4de74
trikunc <kurookun@gmail.com>
2022-04-26 08:45:00 +0700

    heroku 4

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   "presets": ["next/babel"]
-// }
-
-// module.exports = nextConfig
-
-module.exports = {
-  basePath: "",
-  future: {
-    webpack5: false
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  "presets": ["next/babel"]
 }
+
+module.exports = nextConfig
