5ef852c718147a0aa59235e7acf2cab9e2240880
trikunc <kurookun@gmail.com>
2022-04-26 09:10:50 +0700

    try to fix 8

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  "presets": ["next/babel"]
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   "presets": ["next/babel"]
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  basePath: "",
+  future: {
+    webpack5: false
+  }
+}
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import Bcrypt from 'bcrypt'
+// import Bcrypt from 'bcrypt'
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
